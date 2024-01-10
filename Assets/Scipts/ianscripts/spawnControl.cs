ea973d79aa2bbf5bef03f276f6406b84d89d83f4
ian427 <ianunrestricted@gmail.com>
2024-01-10 10:51:21 +0000

    spawnscript
    not quite working but not telling me what the errors are

---
+++ Assets/Scipts/ianscripts/spawnControl.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnControl : MonoBehaviour
+{
+    
+    public GameObject Danger;
+    
+    Camera cam;
+    float Width;
+    float Height;
+    Vector3 topright;
+    Vector3 topleft;
+    Vector3 bottomright;
+    Vector3 bottomleft;
+   float time = 1.5f ;
+    [SerializeField]
+    private bool speedUp=true;
+    private bool spawn;
+    int loopCount = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ScreenSize();
+        ScreenCorners();
+        spawn = true;
+    }
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+    void ScreenSize()
+    {
+        Width = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).x - 0.5f);
+        Height = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).y - 0.5f);
+
+    }
+    void ScreenCorners()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        Vector2 TopRight = new Vector2(Width / 2, Height / 2);// finds the locations of the screens corners
+        Vector2 BottomRight = new Vector2(Width / 2, -Height / 2);
+        Vector2 BottomLeft = new Vector2(-Width / 2, -Height / 2);
+        Vector2 TopLeft = new Vector2(-Width / 2, Height / 2);
+        topright = TopRight;
+        topleft = TopLeft;
+        bottomright = BottomRight;
+        bottomleft = BottomLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawn == true)//prevents couroutine being called every update
+        {
+            StartCoroutine(MoveSpawnPoint());
+            spawn = false;
+        }
+        if (speedUp == true)//prevents couroutine being called every update
+        {
+            StartCoroutine(speedUpC());
+            speedUp = false;
+        }
+    }
+
+
+
+
+    private IEnumerator MoveSpawnPoint()
+    {
+        for (int i = 0; i < loopCount; i++)
+
+            yield return new WaitForSeconds(time * Time.deltaTime); //adds delay
+
+        Instantiate(Collectable, transform.position, transform.rotation);
+        Vector3 randomSP = new Vector3((Width / 2 + 1), UnityEngine.Random.Range(bottomright.y, topright.y), 0); //picks random spot moves to
+        transform.position = randomSP;
+        Instantiate(Danger, transform.position, transform.rotation);
+    }
+   
+    
+    StartCoroutine(speedUpC());
+    spawn = true;//resets loop
+                     // Debug.Log("moved");
+    }
+    private IEnumerator speedUpC()
+    {
+        yield return new WaitForSeconds(0);
+        time -= 0.5f;
+        loopCount += 5;
+        // speedUp = true;
+    }
+}
