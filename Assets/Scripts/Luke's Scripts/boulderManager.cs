da5be20fdebf4564685060f27d3d2d4ca5333c99
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2024-01-16 11:16:00 +0000

    Boulder manager comments

--- Assets/Scripts/Luke's Scripts/boulderManager.cs
+++ Assets/Scripts/Luke's Scripts/boulderManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class boulderManager : MonoBehaviour
 {
+    //Where boulder resets to on x axis
     public int resetPositionX = 40;
 
+    //On trigger, boulder teleports to (40, random y). 
     public void OnTriggerEnter2D (Collider2D col)
     {
         col.transform.position = new Vector2(resetPositionX, Random.Range(-10.0f, 10.0f));
