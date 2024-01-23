3055fa8f83271c9d38f64d331f1e56c5ea248b71
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2024-01-23 09:36:02 +0000

    Player death

--- Assets/Scripts/ianscripts/Playermovement.cs
+++ Assets/Scripts/ianscripts/Playermovement.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Playermovement : MonoBehaviour
 {
     public float movementSpeed;
-    
+
     //Grabs the rigidbody of the sprite, which allows it to move the way it should
-    
-  
+
+
     //Tells the player they can only move side to side at a certain speed
     void Update()
     {
@@ -24,6 +25,12 @@ public class Playermovement : MonoBehaviour
             transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
         }
     }
-
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+            Debug.Log("test");
+            SceneManager.LoadScene("Game Over");
+    }
 }
+
+
 
